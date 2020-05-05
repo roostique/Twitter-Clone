bd6785bb3805e13164625b5a55bf4b3765d06cb9
rustemsu <rustem.supayev@gmail.com>
2020-05-05 19:04:17 +0600

    Initial commit

--- Twitter/Controllers/MainTabController.swift
+++ Twitter/Controllers/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  Twitter
 //
 //  Created by Rustem Supayev on 05/05/2020.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .cyan
     }
+    
 
-
+   
 }
-
