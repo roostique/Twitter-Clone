1761246b8b584c9dac82bf774628c4b53d2cc79a
rustemsu <rustem.supayev@gmail.com>
2020-05-05 18:44:18 +0600

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Rustem Supayev on 05/05/2020.
+//  Copyright © 2020 Rustem Supayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
